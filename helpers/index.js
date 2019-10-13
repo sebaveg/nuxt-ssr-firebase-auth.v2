e9918893053e42c9272f6b8f91f574956a5fc28a
Sebastian Cardoso <sebastiancardoso92@gmail.com>
2019-10-13 18:00:10 -0300

    Not need conditional req.headers.cookie

--- helpers/index.js
+++ helpers/index.js
@@ -4,17 +4,15 @@ var cookieparser = require('cookieparser')
 export function getUserFromCookie (req) {
   if (process.server && process.static) return
   if (!req.headers.cookie) return
+  
+  const parsed = cookieparser.parse(req.headers.cookie)
+  const accessTokenCookie = parsed.access_token
+  if (!accessTokenCookie) return
 
-  if (req.headers.cookie) {
-    const parsed = cookieparser.parse(req.headers.cookie)
-    const accessTokenCookie = parsed.access_token
-    if (!accessTokenCookie) return
+  const decodedToken = jwtDecode(accessTokenCookie)
+  if (!decodedToken) return
 
-    const decodedToken = jwtDecode(accessTokenCookie)
-    if (!decodedToken) return
-
-    return decodedToken
-  }
+  return decodedToken
 }
 
 export function getUserFromSession (req) {
